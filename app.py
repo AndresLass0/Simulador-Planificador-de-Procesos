2c8abbada37fb05dcf1feb46da48495ac6baeca5
Andres Lasso <andreslasso737@gmail.com>
2025-10-21 15:21:23 -0500

    Add Update

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/') #Define la ruta
+def index(): #index es la ruta por defecto que llama flask
+    return render_template('index.html') #Manda la ruta a https::algo/index.html
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
